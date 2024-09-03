2e28149b88cf46617b6884ec54333d48e28db54e
Nerd <studytime023@gmail.com>
2024-09-03 17:09:48 +0300

    chore (Application) : add fonts

---
+++ app/ui/fonts.js
@@ -0,0 +1,22 @@
+import { Black_Ops_One, Courier_Prime, Cutive_Mono } from "next/font/google";
+
+export const blackOpsOne=Black_Ops_One({
+    weight: "400",
+    display: "swap",
+    subsets: ["latin"],
+    fallback: ["monospace"],
+});
+
+export const courierPrime=Courier_Prime({
+    weight: "700",
+    display: "swap",
+    subsets: ["latin"],
+    fallback: ["monospace"],
+});
+
+export const cutiveMono=Cutive_Mono({
+    weight: "400",
+    display: "swap",
+    subsets: ["latin"],
+    fallback: ["monospace"],
+});
