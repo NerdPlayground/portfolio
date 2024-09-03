433b8ccaf5c4781d7012faaf6dc4789b3426a2ee
Nerd <studytime023@gmail.com>
2024-09-03 12:08:09 +0300

    chore (Application) : clean setup files

--- commands.sh
+++
@@ -1,7 +0,0 @@
-rm -f README.md
-rm -rf app public
-mkdir app public app/ui
-touch app/page.js app/layout.js
-touch app/ui/styles.scss app/ui/media-queries.scss app/ui/_functions.scss
-pnpm add --save-dev sass
-pnpm add next@canary
