ae8b20120938ed6aa3ad63545077a652cd690414
Nerd <studytime023@gmail.com>
2024-09-11 00:49:00 +0300

    chore (Page) : add UI loader

---
+++ app/loading.js
@@ -0,0 +1,11 @@
+export default function Loading(){
+    return (
+        <div className="loader">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    );
+}
