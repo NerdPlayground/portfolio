6b33243bb9642e74211b85c18bdf4cc3c86ee61c
Nerd <studytime023@gmail.com>
2024-09-10 23:50:34 +0300

    refractor (Component) : reduce props drilling

--- app/page.js
+++ app/page.js
@@ -1,40 +1,10 @@
-"use client";
 import Content from "./content/main-content";
 import Navigation from "./navigation/navbar";
 
 export default function Root(){
-    /**
-     * closes nav pane if user clicks outside it
-    */
-    function closeNavPane(event){
-        const path=event.composedPath();
-        if(path.some(element=>
-            element.id==="nav-pane" ||
-            (element.classList && element.classList.contains("toggle-nav-pane"))
-        )) return;
-        toggleNavPane();
-    }
-
-    /**
-     * toggles navigation pane
-     * 0px when visible
-     * -250px when hidden
-     */
-    function toggleNavPane(event){
-        let navPane=document.getElementById("nav-pane");
-        if(navPane.style.right==="" || navPane.style.right==="-250px"){
-            navPane.style.right="0px";
-            document.addEventListener("click",closeNavPane);
-        }
-        else if(navPane.style.right==="0px"){
-            document.removeEventListener("click",closeNavPane);
-            navPane.style.right="-250px";
-        }
-    }
-
     return(
         <>
-            <Navigation toggleNavPane={toggleNavPane}/>
+            <Navigation/>
             <Content/>
         </>
     );
