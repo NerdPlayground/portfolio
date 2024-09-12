87886760d8c85a196fa0d102119ccdb6cf7eae76
Nerd <studytime023@gmail.com>
2024-09-12 15:30:01 +0300

    chore (Application) : include metadata

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,13 @@ import "@/app/ui/styles.scss";
 import "@/app/ui/media-queries.scss";
 
 export const metadata = {
-    title: "",
-    description: "",
+    title: "George Mobisa",
+    description: "Get to know a little bit more about the man behind the name",
+    keywords:[
+        "web development","freelance web developer",
+        "frontend developer","Next.js web development",
+        "backend developer","RESTful API development",
+    ]
 };
   
 export default function RootLayout({ children }) {
