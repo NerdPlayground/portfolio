8cb317d53b9046911e3e82721d3eb1992c6b670f
Nerd <studytime023@gmail.com>
2024-09-13 16:47:36 +0300

    fix (Opengraph) : use dynamic image

--- app/layout.js
+++ app/layout.js
@@ -4,11 +4,16 @@ import "@/app/ui/media-queries.scss";
 export const metadata = {
     title: "George Mobisa",
     description: "Get to know a little bit more about the man behind the name",
+    metadataBase: process.env.WEBSITE_URL,
     keywords:[
         "web development","freelance web developer",
         "frontend developer","Next.js web development",
         "backend developer","RESTful API development",
-    ]
+    ],
+    openGraph:{
+        url: process.env.WEBSITE_URL,
+        siteName: "George Mobisa's Portfolio",
+    }
 };
   
 export default function RootLayout({ children }) {
---
+++ app/opengraph-image.js
@@ -0,0 +1,24 @@
+import { ImageResponse } from 'next/og';
+
+export const contentType = 'image/png'
+export const size={ width: 1200, height: 630 };
+export const alt="Software Developer | Frontend 🎨 | Backend 🛠 | Fullstack 🤩";
+
+export default async function Image(){
+    return new ImageResponse(
+        (<div style={{
+            background: "#F5F5F5",
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundSize: "contain",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundImage: `url(${process.env.WEBSITE_URL}/opengraph/developer-activity-amico.svg)`,
+        }}>
+        </div>),
+        {...size}
+    );
+}
