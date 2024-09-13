091e333e4457791a4e8b6301cdf8e9cf1114d3ea
Nerd <studytime023@gmail.com>
2024-09-13 20:06:37 +0300

    fix (Opengraph) : update image styles

--- app/opengraph-image.js
+++ app/opengraph-image.js
@@ -1,5 +1,4 @@
 import { ImageResponse } from 'next/og';
-import { PortfolioPreview } from './lib/components';
 
 export const contentType = 'image/png'
 export const size={ width: 1200, height: 630 };
@@ -9,15 +8,17 @@ export default async function Image(){
     return new ImageResponse(
         (<div style={{
             position: "relative",
-            objectFit: "cover",
             width: "100%",
             height: "100%",
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
             backgroundColor: "#F5F5F5",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundImage: `url(${process.env.WEBSITE_URL}/opengraph/opengraph-image.png)`,
         }}>
-            <PortfolioPreview/>
         </div>),
         {...size}
     );
